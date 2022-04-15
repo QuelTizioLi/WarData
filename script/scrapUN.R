d251b1130bc7e4bea6743a2fa641470214665b8a
vi-enne <44818674+vi-enne@users.noreply.github.com>
2022-04-15 10:50:19 -0400

    Update scrapUN.R

--- script/scrapUN.R
+++ script/scrapUN.R
@@ -20,10 +20,10 @@ n <- nrow(data) #numero tweet da processare
 data$text <- gsub("\\,", "", data$text) #rimuove le virgole
 
 res <- data.frame(matrix(NA, nrow = n, ncol = 7))
-colnames(res) <- c("Data", "Civilian", "KilledTotal", "KilledChildren", "InjuredTotal", "InjuredChildren", "Link")
+colnames(res) <- c("Date", "Civilian", "KilledTotal", "KilledChildren", "InjuredTotal", "InjuredChildren", "Link")
 
 for (i in 1:n) {
-  res$Data[i]   <- substr(as.character(data$created_at[i]), 1, 10)
+  res$Date[i]   <- substr(as.character(data$created_at[i]), 1, 10)
   res$Civilian[i] <- as.numeric(str_extract(data$text[i], "[0-9]+(?=\\s*civilian)"))
   res$KilledTotal[i]  <- as.numeric(str_extract(data$text[i], "[0-9]+(?=\\s*killed)"))
   res$KilledChildren[i] <- as.numeric(str_extract(data$text[i], "[0-9]+(?=\\s*children;)"))
